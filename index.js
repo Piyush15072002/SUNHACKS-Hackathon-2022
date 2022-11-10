202b793947d57042faceb4da672d6c7a22b31a9c
PIYUSH PANT <piyushpant15@gmail.com>
2022-11-11 00:25:23 +0530

    Server Started

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+if (process.env.NODE_ENV !== 'production') {  // if we are not in production mode means we are in development mode then
+    require('dotenv').config();
+}
+
 // Importing libraries and packages
 const express = require('express');
 
 const app = express();
+
+const path = require('path');
+
+const ejsMate = require('ejs-mate'); // lets us create partials, boilerplate etc and add css in ejs files
+
+const mongoose = require('mongoose');
+
+app.engine('ejs', ejsMate);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
