e59871678bf7b875cec7b18860c76ef899e7bc7f
PIYUSH PANT <piyushpant15@gmail.com>
2022-11-11 03:03:41 +0530

    Creating model

--- index.js
+++ index.js
@@ -13,9 +13,14 @@ const ejsMate = require('ejs-mate'); // lets us create partials, boilerplate etc
 
 const mongoose = require('mongoose');
 
+const session = require('express-session');
+
+const cookieparser = require('cookie-parser');
+
 app.engine('ejs', ejsMate);
 
 app.set('view engine', 'ejs');
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
@@ -23,8 +28,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+// Database Connnection
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/sunhacks';
 
-// Routes
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection
+db.on('error', () => {
+    console.error.bind(console, "Connection Error :-<")
+})
+db.once('open', () => {
+    console.log("Connected to Database")
+})
+
+app.use(cookieparser());
+app.use(express.json());
+
+// Router routes importing
+
+const userRoutes = require('./routes/users');
+const guidesRoutes = require('./routes/guides');
+
+// Router Routes handling
+app.use('/user', userRoutes);
+app.use('/guide', guidesRoutes);
+
+
+
+
+// General Routes
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
@@ -33,6 +65,7 @@ app.get('/', (req, res) => {
 app.get('/aboutus', (req, res) => {
     res.render('aboutus.ejs');
 });
+
 
 
 
