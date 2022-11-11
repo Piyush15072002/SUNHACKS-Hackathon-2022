9fcaf115ebbe960e211e5225ef1faf7e1c540de3
PIYUSH PANT <piyushpant15@gmail.com>
2022-11-12 02:24:11 +0530

    Done

--- routes/sites.js
+++ routes/sites.js
@@ -4,14 +4,48 @@ const router = express.Router();
 
 const User = require('../model/User');
 const Guide = require('../model/Guide');
+const Site = require('../model/Sites');
 
 const isVerified = require('../middlewares/isVerified');
 const isLoggedIn = require('../middlewares/isLoggedIn');
 
-router.get('/', (req, res) => {
-    res.render('sites.ejs');
+
+// to visit all sites
+router.get('/', async (req, res) => {
+
+    const sites = await Site.find({});
+
+    res.render('sites.ejs', { sites: sites });
 });
 
+// to add a new site
+router.get('/new', isLoggedIn, async (req, res) => {
+    res.render('newSite.ejs')
+})
+
+router.post('/new', isLoggedIn, async (req, res) => {
+
+    try {
+
+        const { sitename, built, description, city, image } = req.body
+
+        const site = new Site({
+            sitename, built, description, city, image
+        })
+
+        await site.save();
+
+        console.log("Site saved successfully");
+
+        return res.redirect("/sites");
+
+
+    } catch (e) {
+        console.log(e.message);
+        res.send("Error!")
+    }
+
+});
 
 
 
